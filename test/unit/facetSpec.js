009570f263d8d90156237cc0bb90acbcc50f7380
Misty De Meo <mistydemeo@gmail.com>
2015-05-15 11:53:26 -0700

    facetSpec: use toEqual, not toBe, for objects

--- test/unit/facetSpec.js
+++ test/unit/facetSpec.js
@@ -6,7 +6,7 @@ describe("Facet", function() {
   it("should allow new facets to be defined by name", inject(function(Facet) {
     expect(Facet.get("foo")).toBe(undefined);
     Facet.add("foo", "bar");
-    expect(Facet.get("foo")).toBe(["bar"]);
+    expect(Facet.get("foo")).toEqual(["bar"]);
   }));
 
   it("should allow a specific facet to be removed", inject(function(Facet) {
@@ -34,12 +34,12 @@ describe("Facet", function() {
 
   it("should allow facets to be defined as strings to perform exact matches", inject(function(Facet) {
     Facet.add("puid", "fmt/256");
-    expect(Facet.filter([{"puid": "fmt/256"}, {"puid": "fmt/128"}, {"puid": "fmt/2560"}])).toBe([{"puid": "fmt/256"}]);
+    expect(Facet.filter([{"puid": "fmt/256"}, {"puid": "fmt/128"}, {"puid": "fmt/2560"}])).toEqual([{"puid": "fmt/256"}]);
   }));
 
   it("should allow facets to be defined as regular expressions to perform fuzzy matches", inject(function(Facet) {
     Facet.add("filename", /\.jpg$/);
-    expect(Facet.filter([{"filename": "1.png"}, {"filename": "2.jpg"}])).toBe([{"filename": "2.jpg"}]);
+    expect(Facet.filter([{"filename": "1.png"}, {"filename": "2.jpg"}])).toEqual([{"filename": "2.jpg"}]);
   }));
 
   it("should allow facets to be defined as functions to perform custom matches", inject(function(Facet) {
@@ -47,6 +47,6 @@ describe("Facet", function() {
       var start = value.split(":")[0];
       return Date.parse(start) > Date.parse("1970");
     });
-    expect(Facet.filter([{"date": "1950:1999"}, {"date": "1975:1980"}])).toBe([{"date": "1975:1980"}]);
+    expect(Facet.filter([{"date": "1950:1999"}, {"date": "1975:1980"}])).toEqual([{"date": "1975:1980"}]);
   }));
 });
