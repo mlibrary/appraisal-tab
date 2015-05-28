5bf9cd042fa0c181981cc2bf57f329dd914dc799
Misty De Meo <mistydemeo@gmail.com>
2015-05-28 11:31:08 -0700

    Facet filter: add custom hash function

--- app/filters/facet.filter.js
+++ app/filters/facet.filter.js
@@ -3,9 +3,17 @@
 (function() {
   var facetFilter = angular.module('facetFilter', []);
 
-  facetFilter.filter('facet', function(Facet) {
-    return _.memoize(function(records) {
+  facetFilter.filter('facet', function() {
+    var facet_filter_fn = function(Facet) {
       return Facet.filter(records);
-    });
+    };
+
+    // The default hash function may confuse two arrays of objects
+    // of the same length as the same set of records.
+    var hash_fn = function(records) {
+      JSON.stringify(records);
+    };
+
+    return _.memoize(facet_filter_fn, hash_fn);
   });
 })();
