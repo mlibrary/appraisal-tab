f7c907b881bbfdb123029c1512f1cf02182ead60
Misty De Meo <mistydemeo@gmail.com>
2015-05-28 11:48:16 -0700

    Facet filter: this isn't clojure

--- app/filters/facet.filter.js
+++ app/filters/facet.filter.js
@@ -11,7 +11,7 @@
     // The default hash function may confuse two arrays of objects
     // of the same length as the same set of records.
     var hash_fn = function(records) {
-      JSON.stringify(records);
+      return JSON.stringify(records);
     };
 
     return _.memoize(facet_filter_fn, hash_fn);
