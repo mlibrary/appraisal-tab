674669d050791a09ad71af4e72b4767e4b7ec0cb
Misty De Meo <mistydemeo@gmail.com>
2015-06-01 14:36:09 -0700

    Facet filter: take internal state into account when memoizing

--- app/filters/facet.filter.js
+++ app/filters/facet.filter.js
@@ -11,7 +11,7 @@
     // The default hash function may confuse two arrays of objects
     // of the same length as the same set of records.
     var hash_fn = function(records) {
-      return JSON.stringify(records);
+      return JSON.stringify(records) + JSON.stringify(Facet.facets);
     };
 
     return _.memoize(facet_filter_fn, hash_fn);
