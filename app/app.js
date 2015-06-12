b404ebbced22bb2cafdc07abd752265ea60dcf60
Misty De Meo <mistydemeo@gmail.com>
2015-06-12 12:18:05 -0700

    Add routing, split up views

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@
 
 // Declare app level module which depends on views, and components
 angular.module('appraisalTab', [
+  'ngRoute',
+  'route-segment',
+  'view-segment',
   'angularCharts',
   'restangular',
   'appraisalTab.version',
@@ -20,4 +23,39 @@ angular.module('appraisalTab', [
   'visualizationsController',
 ]).config(function(RestangularProvider) {
     RestangularProvider.setBaseUrl('/app/fixtures');
-});
+}).config(function($routeSegmentProvider) {
+  $routeSegmentProvider.options.autoLoadTemplates = true;
+
+  $routeSegmentProvider.
+    when('/report', 'report').
+    when('/visualizations', 'visualizations').
+    when('/visualizations/files', 'visualizations.files').
+    when('/visualizations/size', 'visualizations.size').
+    when('/archivesspace', 'archivesspace').
+
+    segment('report', {
+      templateUrl: 'report/report.html',
+      controller: 'ReportController',
+    }).
+
+    segment('visualizations', {
+      templateUrl: 'visualizations/visualizations.html',
+      controller: 'VisualizationsController',
+    }).
+    within().
+      segment('files', {
+        default: 'true',
+        templateUrl: 'visualizations/formats_by_files.html',
+      }).
+      segment('size', {
+        templateUrl: 'visualizations/formats_by_size.html',
+      }).
+    up().
+
+    segment('archivesspace', {
+      templateUrl: 'archivesspace/archivesspace.html',
+      controller: 'ArchivesSpaceController',
+    });
+}).controller('MainController', ['$scope', '$routeSegment', function($scope, $routeSegment) {
+  $scope.$routeSegment = $routeSegment;
+}]);
