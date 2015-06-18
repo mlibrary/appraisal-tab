e1342f11d486abe7e7f99442778649108eed78cc
Misty De Meo <mistydemeo@gmail.com>
2015-06-18 15:49:36 -0700

    Visualizations: add facet when clicking format

--- app/visualizations/visualizations.controller.js
+++ app/visualizations/visualizations.controller.js
@@ -1,12 +1,16 @@
 'use strict';
 
 (function() {
-  angular.module('visualizationsController', ['angularCharts', 'selectedFilesService']).controller('VisualizationsController', function($scope, SelectedFiles) {
+  angular.module('visualizationsController', ['angularCharts', 'selectedFilesService']).controller('VisualizationsController', ['$scope', 'Facet', 'SelectedFiles', function($scope, Facet, SelectedFiles) {
     // Displays aggregate information about file formats;
     // the selected record data is filtered/reformatted in the view.
     $scope.records = SelectedFiles;
     $scope.puid_chart_type = 'pie';
     $scope.puid_config = {
+      click: function(d) {
+        Facet.remove('puid');
+        Facet.add('puid', d.data.x);
+      },
       title: 'Formats (total)',
       tooltips: true,
       labels: false,
@@ -18,6 +22,10 @@
 
     $scope.size_chart_type = 'pie';
     $scope.size_config = {
+      click: function(d) {
+        Facet.remove('puid');
+        Facet.add('puid', d.data.x);
+      },
       title: 'Formats (by size)',
       tooltips: true,
       labels: false,
@@ -26,5 +34,5 @@
         position: 'right',
       },
     };
-  });
+  }]);
 })();
