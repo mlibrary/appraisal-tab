abd46b2d0d9c11f8d2a42b10bffd7805b70932aa
Misty De Meo <mdemeo@artefactual.com>
2015-07-03 12:01:30 -0700

    Replace jqTree with angular-tree-control

--- app/archivesspace/archivesspace.controller.js
+++ app/archivesspace/archivesspace.controller.js
@@ -2,30 +2,12 @@
 
 (function() {
   angular.module('archivesSpaceController', []).controller('ArchivesSpaceController', ['$scope', 'ArchivesSpace', function($scope, ArchivesSpace) {
-      var format_item = function(item) {
-        if (item.children && item.children.length > 0) {
-          item.children = item.children.map(function(child) {
-            return format_item(child);
-          });
-        }
-
-        item.label = item.title;
-        if (item.identifier) {
-          item.label += '(' + item.identifier + ')';
-        }
-
-        return item;
-      };
-
       $scope.options = {
-        closedIcon: $('<span class="fa fa-folder-o"></span>'),
-        openedIcon: $('<span class="fa fa-folder-open-o"></span>'),
+        dirSelectable: true,
       };
 
       ArchivesSpace.all().then(function(data) {
-        $scope.data = data.map(function(child) {
-          return format_item(child);
-        });
+        $scope.data = data;
       });
     }]);
 })();
--- app/tree/tree.controller.js
+++ app/tree/tree.controller.js
@@ -6,33 +6,10 @@
   treeController.controller('TreeController', ['$scope', 'SelectedFiles', 'Transfer', function($scope, SelectedFiles, Transfer) {
 
     $scope.options = {
-      closedIcon: $('<span class="fa fa-folder-o"></span>'),
-      openedIcon: $('<span class="fa fa-folder-open-o"></span>'),
+      dirSelectable: true,
+      multiSelection: true,
     };
 
-    $scope.on_click = function(el) {
-      // Disable single selection
-      el.preventDefault();
-
-      var selected_node = el.node;
-      if (this.tree('isNodeSelected', selected_node)) {
-        var removed = this.iterate_and_apply_event_recursively('removeFromSelection', selected_node);
-
-        $scope.$apply(function() {
-          angular.forEach(removed, function(id) {
-            SelectedFiles.remove(id);
-          });
-        });
-      } else {
-        var added = this.iterate_and_apply_event_recursively('addToSelection', selected_node);
-
-        $scope.$apply(function() {
-          angular.forEach(added, function(id) {
-            SelectedFiles.add(id);
-          });
-        });
-      }
-    };
     Transfer.all().then(function(data) {
       $scope.data = data.transfers;
     });
