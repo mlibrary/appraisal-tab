88fa06277cc4eda80b0ca4a61d7886617b34122a
Misty De Meo <mdemeo@artefactual.com>
2015-07-08 15:46:52 -0700

    Tag: track a flat list of all tags

--- app/services/tag.service.js
+++ app/services/tag.service.js
@@ -6,6 +6,8 @@
   factory('Tag', ['$log', 'Transfer', function($log, Transfer) {
     var tags = {};
 
+    // public
+
     var add = function(id, tag) {
       tags[id] = tags[id] || [];
 
@@ -14,6 +16,12 @@
       }
 
       tags[id].push(tag);
+
+      // Add this tag to the flat list of current tags if not already present
+      if (this.tags.indexOf(tag) === -1) {
+        this.tags.push(tag);
+      }
+
       var record = Transfer.id_map[id];
       if (!record) {
         $log.warn('Tried to add tag for file with ID ' + String(id) + ' which was not found in Transfer map');
@@ -24,12 +32,25 @@
     };
 
     var add_list = function(ids, tag) {
+      var self = this;
       angular.forEach(ids, function(id) {
-        add(id, tag);
+        self.add(id, tag);
       });
     };
 
     var remove = function(id, tag) {
+      if (tag === undefined) {
+        var self = this;
+        var tags_for_id = tags[id];
+
+        remove_all(id);
+        angular.forEach(tags_for_id, function(tag) {
+          remove_tag_if_necessary(self, tag);
+        });
+
+        return;
+      }
+
       if (tags[id] === undefined) {
         $log.warn('Tried to remove tag for file with ID ' + String(id) + ' but no tags are specified for that file');
         return;
@@ -44,14 +65,9 @@
       if (record.tags) {
         record.tags.pop(tag);
       }
-    };
 
-    var clear = function(id) {
-      tags[id] = [];
-      var record = Transfer.id_map[id];
-      if (record) {
-        record.tags = [];
-      }
+      // If this is the last occurrence of this tag, delete it from the tag list
+      remove_tag_if_necessary(self, tag);
     };
 
     var get = function(id) {
@@ -69,6 +85,29 @@
       return results;
     };
 
+    var clear = function() {
+      tags = {};
+      this.tags = [];
+    };
+
+    // private
+
+    var remove_tag_if_necessary = function(self, tag) {
+      if (self.list(tag).length === 0) {
+        self.tags = self.tags.filter(function(element) {
+          return element !== tag;
+        });
+      }
+    };
+
+    var remove_all = function(id) {
+      tags[id] = [];
+      var record = Transfer.id_map[id];
+      if (record) {
+        record.tags = [];
+      }
+    };
+
     return {
       add: add,
       add_list: add_list,
@@ -76,6 +115,7 @@
       remove: remove,
       clear: clear,
       list: list,
+      tags: [],
     };
   }]);
 })();
--- test/unit/tagSpec.js
+++ test/unit/tagSpec.js
@@ -2,6 +2,9 @@
 
 describe('Tag', function() {
   beforeEach(module('tagService'));
+  beforeEach(angular.mock.inject(function(Tag) {
+    Tag.clear();
+  }));
 
   it('should be able to track tags for a given file', inject(function(Tag) {
     Tag.add('2d3fbf28-0102-4de7-9bd0-77af2808d5f0', 'testtag');
@@ -21,11 +24,11 @@ describe('Tag', function() {
     expect(Tag.get('f2c52912-0297-4f7a-90a7-f7a9323eeb5d')).toEqual([]);
   }));
 
-  it('should be able to clear tags for a given file', inject(function(Tag) {
+  it('should be able to remove all tags for a given file if no tag is specified', inject(function(Tag) {
     Tag.add('f9416832-13df-411a-b0ad-ad994689b760', 'testtag1');
     Tag.add('f9416832-13df-411a-b0ad-ad994689b760', 'testtag2');
     expect(Tag.get('f9416832-13df-411a-b0ad-ad994689b760')).toEqual(['testtag1', 'testtag2']);
-    Tag.clear('f9416832-13df-411a-b0ad-ad994689b760');
+    Tag.remove('f9416832-13df-411a-b0ad-ad994689b760');
     expect(Tag.get('f9416832-13df-411a-b0ad-ad994689b760')).toEqual([]);
   }));
 
@@ -34,5 +37,14 @@ describe('Tag', function() {
     Tag.add('87c16fac-01a5-444c-b88a-e1de8cb9500d', 'testtag');
     Tag.add('3661b2ba-4341-45a6-bb74-b41e95eaeb7a', 'testtag');
     expect(Tag.list('testtag')).toEqual(['87c16fac-01a5-444c-b88a-e1de8cb9500d', '3661b2ba-4341-45a6-bb74-b41e95eaeb7a']);
+  }));
+
+  it('should publish a flat list of every specified tag', inject(function(Tag) {
+    expect(Tag.tags).toEqual([]);
+    Tag.add('79009039-e221-42d0-b093-c61357a272ba', 'testtag1');
+    Tag.add('bdec60f5-deb5-4d04-a488-96cd5702225e', 'testtag2');
+    expect(Tag.tags).toEqual(['testtag1', 'testtag2']);
+    Tag.remove('79009039-e221-42d0-b093-c61357a272ba');
+    expect(Tag.tags).toEqual(['testtag2']);
   }));
 });
