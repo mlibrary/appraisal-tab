58bf15cadbccb4d9c76f697de07ebca0f0bcea19
Misty De Meo <mdemeo@artefactual.com>
2015-07-09 19:05:48 -0700

    update selectedFilesSpec for new behaviour

--- test/unit/selectedFilesSpec.js
+++ test/unit/selectedFilesSpec.js
@@ -2,43 +2,41 @@
 
 describe('SelectedFiles', function() {
   beforeEach(module('selectedFilesService'));
-  beforeEach(angular.mock.inject(function(_$httpBackend_) {
-    _$httpBackend_.when('GET', '/file/054a0f2c-79bb-4051-b82b-4b0f14564811').respond({
-      'id': '054a0f2c-79bb-4051-b82b-4b0f14564811',
-      'text': 'lion.svg',
-    });
-    _$httpBackend_.when('GET', '/file/939110fa-8c73-4531-8aa0-aa28e90ca108').respond({
-      'id': '939110fa-8c73-4531-8aa0-aa28e90ca108',
-      'text': 'garnet.jpg',
-    });
-    _$httpBackend_.when('GET', '/file/3400c608-4dbf-4eeb-af22-7fb4ca6a5de6').respond({
-      'id': '3400c608-4dbf-4eeb-af22-7fb4ca6a5de6',
-      'text': 'amethyst.tif',
-    });
-  }));
+
+  var test_item_1 = {
+    'id': '054a0f2c-79bb-4051-b82b-4b0f14564811',
+    'text': 'lion.svg',
+  };
+
+  var test_item_2 = {
+    'id': '939110fa-8c73-4531-8aa0-aa28e90ca108',
+    'text': 'garnet.jpg',
+  };
+
+  var test_item_3 = {
+    'id': '3400c608-4dbf-4eeb-af22-7fb4ca6a5de6',
+    'text': 'amethyst.tif',
+  };
+
   beforeEach(angular.mock.inject(function(SelectedFiles) {
     SelectedFiles.selected = [];
   }));
 
   it('should allow files to be specified by UUID', inject(function(_$httpBackend_, SelectedFiles) {
-    SelectedFiles.add('054a0f2c-79bb-4051-b82b-4b0f14564811');
-    _$httpBackend_.flush();
+    SelectedFiles.add(test_item_1);
     expect(SelectedFiles.selected.length).toEqual(1);
   }));
 
   it('should allow files to be removed by UUID', inject(function(_$httpBackend_, SelectedFiles) {
-    SelectedFiles.add('054a0f2c-79bb-4051-b82b-4b0f14564811');
-    _$httpBackend_.flush();
+    SelectedFiles.add(test_item_1);
     expect(SelectedFiles.selected.length).toEqual(1);
     SelectedFiles.remove('054a0f2c-79bb-4051-b82b-4b0f14564811');
     expect(SelectedFiles.selected.length).toEqual(0);
   }));
 
   it('should allow files to be removed selectively', inject(function(_$httpBackend_, SelectedFiles) {
-    SelectedFiles.add('054a0f2c-79bb-4051-b82b-4b0f14564811');
-    _$httpBackend_.flush();
-    SelectedFiles.add('939110fa-8c73-4531-8aa0-aa28e90ca108');
-    _$httpBackend_.flush();
+    SelectedFiles.add(test_item_1);
+    SelectedFiles.add(test_item_2);
     SelectedFiles.remove('054a0f2c-79bb-4051-b82b-4b0f14564811');
     expect(SelectedFiles.selected.length).toEqual(1);
     expect(SelectedFiles.selected[0].text).toEqual('garnet.jpg');
@@ -46,8 +44,7 @@ describe('SelectedFiles', function() {
 
   it('should be able to return a list of IDs', inject(function(_$httpBackend_, SelectedFiles) {
       expect(SelectedFiles.list_ids().length).toEqual(0);
-      SelectedFiles.add('3400c608-4dbf-4eeb-af22-7fb4ca6a5de6');
-      _$httpBackend_.flush();
+      SelectedFiles.add(test_item_3);
       expect(SelectedFiles.list_ids()).toEqual(['3400c608-4dbf-4eeb-af22-7fb4ca6a5de6']);
     }));
 });
