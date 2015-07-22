d107aad461f193a8fa4c4e76c6a95f67182c0b01
Misty De Meo <mdemeo@artefactual.com>
2015-07-22 15:07:32 -0700

    File list: display, allow removing tags

--- app/file_list/file_list.controller.js
+++ app/file_list/file_list.controller.js
@@ -3,8 +3,11 @@
 (function() {
   angular.module('fileListController', ['fileListService']).
 
-  controller('FileListController', ['$scope', 'FileList', function($scope, FileList) {
+  controller('FileListController', ['$scope', 'FileList', 'Tag', function($scope, FileList, Tag) {
     $scope.file_list = FileList;
     $scope.remove_file = FileList.remove.bind(FileList);
+    $scope.remove_tag = function(id, tag) {
+      Tag.remove(id, tag);
+    };
   }]);
 })();
