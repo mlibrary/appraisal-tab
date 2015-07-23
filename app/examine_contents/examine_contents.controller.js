da30889cded5232f182f041f1ff08ce43d10529b
Misty De Meo <mdemeo@artefactual.com>
2015-07-23 14:53:16 -0700

    Examine contents: add tag input box

--- app/examine_contents/examine_contents.controller.js
+++ app/examine_contents/examine_contents.controller.js
@@ -3,10 +3,23 @@
 (function() {
   angular.module('examineContentsController', []).
 
-  controller('ExamineContentsController', ['$scope', '$routeSegment', 'SelectedFiles', function($scope, $routeSegment, SelectedFiles) {
+  controller('ExamineContentsController', ['$scope', '$routeSegment', 'SelectedFiles', 'Tag', function($scope, $routeSegment, SelectedFiles, Tag) {
     $scope.$routeSegment = $routeSegment;
     $scope.type = $routeSegment.$routeParams.type;
     $scope.SelectedFiles = SelectedFiles;
+
+    $scope.submit = function(files) {
+      var tag = this.tag;
+      if (!tag) {
+        return;
+      }
+
+      var ids = files.map(function(file) {
+        return file.id;
+      });
+      Tag.add_list(ids, tag);
+      this.tag = '';
+    };
   }]).
 
   controller('ExamineContentsFileController', ['$scope', '$routeSegment', 'File', function($scope, $routeSegment, File) {
