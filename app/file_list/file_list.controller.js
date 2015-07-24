b66162039155a1da0102ada13624fba8380eeefe
Misty De Meo <mdemeo@artefactual.com>
2015-07-24 11:41:49 -0700

    File list: add tag box to tag selected files

--- app/file_list/file_list.controller.js
+++ app/file_list/file_list.controller.js
@@ -9,5 +9,49 @@
     $scope.remove_tag = function(id, tag) {
       Tag.remove(id, tag);
     };
+
+    // Ensure the "selected" list is empty if the file list changes
+    $scope.watch('file_list', function() {
+      $scope.$apply(function() {
+        $scope.selected = [];
+        $scope.all_selected = false;
+      });
+    });
+
+    $scope.selected = [];
+    $scope.all_selected = false;
+
+    $scope.select_all = function() {
+      if (!$scope.all_selected) {
+        $scope.selected = FileList.files.map(function(file) {
+          return file.id;
+        });
+        $scope.all_selected = true;
+      } else {
+        $scope.selected = [];
+        $scope.all_selected = false;
+      }
+    };
+    $scope.toggle_file = function(uuid) {
+      var index = $scope.selected.indexOf(uuid);
+      // remove from selection
+      if (index > -1) {
+        $scope.selected.splice(index, 1);
+      } else {
+        $scope.selected.push(uuid);
+      }
+
+      $scope.all_selected = !($scope.selected.length < FileList.files.length);
+    };
+
+    $scope.submit = function(uuids) {
+      var tag = this.tag;
+      if (!tag) {
+        return;
+      }
+
+      Tag.add_list(uuids, tag);
+      this.tag = '';
+    };
   }]);
 })();
