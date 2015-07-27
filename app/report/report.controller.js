583f1b6604728aa601f9b8c1ce44161362bc39ed
Misty De Meo <mdemeo@artefactual.com>
2015-07-27 16:43:22 -0700

    Report: allow sorting table

--- app/report/report.controller.js
+++ app/report/report.controller.js
@@ -5,5 +5,25 @@
 
   controller('ReportController', ['$scope', 'SelectedFiles', function($scope, SelectedFiles) {
     $scope.records = SelectedFiles;
+
+    $scope.sort_property = 'format';
+    $scope.reverse = false;
+    $scope.sort_fn = function(record) {
+      var sort_prop = $scope.sort_property;
+      if (sort_prop === 'puid') {
+        return record.puid;
+      } else {
+        return record.data[sort_prop];
+      }
+    };
+
+    $scope.set_sort_property = function(property) {
+      if ($scope.sort_property === property) {
+        $scope.reverse = !$scope.reverse;
+      } else {
+        $scope.reverse = false;
+        $scope.sort_property = property;
+      }
+    };
   }]);
 })();
