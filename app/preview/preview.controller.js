efcd1a14d2326185154cfe5d680d366ee62dbfdb
Misty De Meo <mdemeo@artefactual.com>
2015-08-11 10:16:03 -0700

    Preview: get file data from File if not in FileList
    The file data requested may not be located in the FileList. This
    happens if, for example, the user is visiting the appraisal tab from
    a link to the preview pane which contains a file UUID; the FileList
    will be empty in this case.
    
    This still fetches the file from the FileList where possible, but will
    fall back to fetching it from the server otherwise.

--- app/preview/preview.controller.js
+++ app/preview/preview.controller.js
@@ -3,14 +3,27 @@
 (function() {
   angular.module('previewController', ['route-segment', 'fileListService']).
 
-  controller('PreviewController', ['$scope', '$routeSegment', 'FileList', function($scope, $routeSegment, FileList) {
+  controller('PreviewController', ['$scope', '$routeSegment', 'File', 'FileList', function($scope, $routeSegment, File, FileList) {
+    var vm = this;
+
+    vm.set_file_data = function(file) {
+      $scope.file = file;
+      $scope.url = 'fixtures/content/' + file.title;
+    };
+
     $scope.$routeSegment = $routeSegment;
     $scope.id = $routeSegment.$routeParams.id;
     if ($scope.id !== undefined) {
+      // Try to get file from the file list first, to avoid pinging the server.
       var file = FileList.get($scope.id);
       if (file) {
-        $scope.file = file;
-        $scope.url = 'fixtures/content/' + file.title;
+        vm.set_file_data(file);
+      } else {
+        // If the data isn't available, contact the server to fetch file info;
+        // it may not have been loaded.
+        File.get($scope.id).then(function (file) {
+          vm.set_file_data(file);
+        });
       }
     }
   }]);
