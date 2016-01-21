9691e7713b93d5fb5125705bf5ebe9993592e662
Misty De Meo <mdemeo@artefactual.com>
2016-01-21 12:17:18 -0800

    Include polyfill for Object.assign
    This is available in current versions of major browsers, but including
    the polyfill would be useful in case users are on older versions.

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel?presets[]=es2015',
+        loader: 'babel?presets[]=es2015,plugins=transform-object-assign',
         exclude: /node_modules/,
       },
       {
