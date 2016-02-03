3c3992a32c302c623b78e0b5e2178c8d226385f8
Misty De Meo <mdemeo@artefactual.com>
2016-02-03 15:37:32 -0800

    Visualizations controller: add comments

--- app/visualizations/visualizations.controller.js
+++ app/visualizations/visualizations.controller.js
@@ -5,11 +5,14 @@ angular.module('visualizationsController', [
   'selectedFilesService',
 ]).
 
+// Feeds data into the graphs in the two visualization partials.
 controller('VisualizationsController', ['$scope', 'Facet', 'SelectedFiles', 'Transfer', function($scope, Facet, SelectedFiles, Transfer) {
   // Displays aggregate information about file formats;
   // the selected record data is filtered/reformatted in the view.
   $scope.records = SelectedFiles;
   $scope.format_chart_type = 'pie';
+  // Check angular-charts for the supported options:
+  // https://github.com/chinmaymk/angular-charts
   $scope.format_config = {
     // Formats (total)
     click: record => {
