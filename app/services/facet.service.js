9145ca8ff4f19ab0d948cc1a009e35574cb25344
Misty De Meo <mdemeo@artefactual.com>
2016-02-03 10:12:11 -0800

    Facet: add API documentation

--- app/services/facet.service.js
+++ app/services/facet.service.js
@@ -2,7 +2,28 @@ import angular from 'angular';
 
 angular.module('facetService', []).
 
+// Provides the ability to filter sets of data via various criteria.
+// The Facet service operates on lists of objects, and filters them
+// according to criteria which evaluates specific keys in those
+// objects.
+// Facets provide several different ways of matching:
+// * string   - if the test value is a string, then the facet only matches if
+//              the object has a value at that key which is an exact match.
+// * regexp   - if the test value is a regexp, then the facet matches if
+//              the object has a value at that key which passes value.match(regexp).
+// * function - if the test value is a function, then the facet matches if
+//              the function passes true when passed the value at that key from the object.
 factory('Facet', function() {
+  // Adds a new facet.
+  // * name - the key this function should match.
+  // * value - the value to match against the object's value; see above.
+  // * data - an object with arbitrary keys/values which can be useful to outside
+  //          systems querying the tag list.
+  //          For example, the facet selector controller uses the `data` property to
+  //          track display-formatted strings which are used in the UI when reading
+  //          data from Facet.facet_list.
+  // * id - if provided, a unique ID for the facet, which allows it to be removed
+  //        by ID in the future.
   var add = function(name, value, data, id) {
     data = data || {};
 
@@ -22,6 +43,10 @@ factory('Facet', function() {
     return data.id;
   };
 
+  // If `value` is provided, returns a facet with this `name` and `value`,
+  // or `undefined` if no matching facet is tracked.
+  // If `value` is not provided, returns all facets with this `name`,
+  // or `undefined` if no matching facet is tracked.
   var get = function(name, value) {
     // If the facet doesn't exist, just return nothing
     if (undefined === this.facets[name]) {
@@ -44,6 +69,8 @@ factory('Facet', function() {
     }
   };
 
+  // Returns a facet with this `name` and `id`,
+  // or undefined if no matching facet is tracked.
   var get_by_id = function(name, id) {
     var facets = this.facets[name].filter(function(element) {
       return element.id === id;
@@ -51,6 +78,9 @@ factory('Facet', function() {
     return facets[0].value;
   };
 
+  // Removes one or more facets.
+  // If `value` is provided, removes a facet that matches both `name` and `value`;
+  // otherwise, removes every facet with `name`.
   var remove = function(name, value) {
     // If no value is provided, delete all values
     if (undefined === value) {
@@ -72,6 +102,7 @@ factory('Facet', function() {
     }
   };
 
+  // Returns a facet with this `name` and `id`.
   var remove_by_id = function(name, id) {
     var filter_fn = function(element) {
       return element.id !== id;
@@ -83,11 +114,17 @@ factory('Facet', function() {
     this.facet_list = this.facet_list.filter(filter_fn);
   };
 
+  // Removes every facet.
   var clear = function() {
     this.facets = {};
     this.facet_list = [];
   };
 
+  // Filters an array of objects.
+  // Each object is tested against the full list of defined facets
+  // using `passes_filters()`, and is only included in the output array
+  // if that returns true.
+  // Returns a copy of the array with only the matching entries.
   var filter = function(values) {
     var self = this; // needs to be defined outside the filter function
     return values.filter(function(element, index, array) {
@@ -95,6 +132,11 @@ factory('Facet', function() {
     });
   };
 
+  // Tests `object` against the currently-defined facets.
+  // The object is not matched against facets defined against keys
+  // which the object doesn't have.
+  // Returns true if the object passes at least one facet defined for its
+  // keys, false otherwise.
   var passes_filters = function(object) {
     var keys = Object.keys(this.facets);
     var key, value;
