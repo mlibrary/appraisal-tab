5eca2c3c0657ec1b2896d107cb8d83eeb96ea8ee
Misty De Meo <mdemeo@artefactual.com>
2016-02-03 15:28:09 -0800

    Preview controller: add comments

--- app/preview/preview.controller.js
+++ app/preview/preview.controller.js
@@ -2,6 +2,14 @@ import angular from 'angular';
 
 angular.module('previewController', ['route-segment', 'selectedFilesService']).
 
+// Used to populate the preview pane, which displays:
+// * the file in an iframe;
+// * the filename;
+// * a download link
+//
+// FIXME If the file being previewed belongs to a SIP that's already been started,
+//       then the preview/download URL will 404. This looks really ugly to the
+//       end user. It would be great to find a more elegant solution to this.
 controller('PreviewController', ['$scope', '$routeSegment', 'Alert', 'File', 'SelectedFiles', function($scope, $routeSegment, Alert, File, SelectedFiles) {
   var vm = this;
 
@@ -11,6 +19,8 @@ controller('PreviewController', ['$scope', '$routeSegment', 'Alert', 'File', 'Se
   };
 
   $scope.$routeSegment = $routeSegment;
+  // The UUID of the file is located in the URL, which allows users to
+  // share the URL with another user to let them preview the file.
   $scope.id = $routeSegment.$routeParams.id;
   if ($scope.id !== undefined) {
     // Try to get file from the file list first, to avoid pinging the server.
