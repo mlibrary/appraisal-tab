92cbc4deb345f635009fd0a332b3945523665794
Misty De Meo <mdemeo@artefactual.com>
2016-02-04 09:44:29 -0800

    ui-minimize-bar directive: add comments

--- app/ui/ui.directive.js
+++ app/ui/ui.directive.js
@@ -2,6 +2,12 @@ import angular from 'angular';
 
 angular.module('uiDirectives', []).
 
+// The <ui-minimize-bar> and <ui-minimize-panel> directives allow the creation of
+// elements which can be displayed and hidden via a set of buttons.
+//
+// <ui-minimize-bar> produces a horizontal div of buttons, one for every
+// <ui-minimize-panel>. Clicking one of those buttons toggles the display
+// of a panel on or off.
 directive('uiMinimizeBar', [function() {
   return {
     restrict: 'E',
@@ -27,6 +33,15 @@ directive('uiMinimizeBar', [function() {
   };
 }]).
 
+// Every <ui-minimize-panel> element produces a new togglable panel.
+// These panels will all be displayed in a row horizontally, and will adjust their
+// size as new panels are added or removed.
+// A button to display/collapse the panel will be generated in the <ui-minimize-bar>
+// element.
+// The "title" attribute controls the display title in the panel's header and in
+// the <ui-minimize-bar>'s button.
+// The "open" attribute, set to "true" or "false", controls whether this panel
+// should be open by default.
 directive('uiMinimizePanel', [function() {
   return {
     require: '^uiMinimizeBar',
