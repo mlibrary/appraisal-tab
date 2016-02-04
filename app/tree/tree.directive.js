20a31e9c923a2ff5778b2e28a07322f5d4f38a9f
Misty De Meo <mdemeo@artefactual.com>
2016-02-04 10:02:15 -0800

    Tree directives: add comments

--- app/tree/tree.directive.js
+++ app/tree/tree.directive.js
@@ -3,6 +3,14 @@ import $ from 'jquery';
 
 angular.module('treeDirectives', []).
 
+// When set as an attribute on an element, makes that element draggable.
+// A tree-draggable element can be dropped onto a tree-droppable element.
+// The "helper" attribute is a helper function which should return an
+// element to render the draggable object as.
+// The helper function's `this` element will be the original element the
+// user started dragging.
+// For example, the transfer backlog's helper is used to produce a simpler
+// element which is easier to see when dragging than the original was.
 directive('treeDraggable', ['$parse', function($parse) {
   return {
     restrict: 'A',
@@ -20,6 +28,12 @@ directive('treeDraggable', ['$parse', function($parse) {
   };
 }]).
 
+// Marks an element as droppable; it will accept any tree-draggable element.
+// The "on-drop" attribute is a function which will be called when an element
+// is dropped onto it. See jquery-ui.droppable's documentation for information
+// on the arguments provided to this function:
+// http://api.jqueryui.com/droppable/#event-drop
+// That function's `this` will be the tree-droppable element.
 directive('treeDroppable', ['$parse', function($parse) {
   return {
     restrict: 'A',
@@ -28,6 +42,10 @@ directive('treeDroppable', ['$parse', function($parse) {
 
       $(element).droppable({
         drop: drop_fn.bind($scope.node),
+        // The default tolerance requires 50% of the draggable element to be
+        // intersecting with the droppable element. This is pretty hard to achieve
+        // with the way we're rendering element names. The "pointer" option checks
+        // for mouse cursor position instead, which is more reliable for us.
         tolerance: 'pointer',
       });
     },
