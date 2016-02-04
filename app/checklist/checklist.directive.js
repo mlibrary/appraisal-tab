c637f49bdf6e0d669b35411a18a41d5aa77d12df
Misty De Meo <mdemeo@artefactual.com>
2016-02-04 10:15:22 -0800

    Checklist directive: add comments

--- app/checklist/checklist.directive.js
+++ app/checklist/checklist.directive.js
@@ -2,6 +2,18 @@ import angular from 'angular';
 
 angular.module('checklistDirective', []).
 
+// The checklist directive simplifies tracking the state of a series of
+// checkboxes, and whether every checkbox in the set is checked.
+//
+// The "ng-element" attribute is a record associated with the checkbox.
+// Its `id` attribute will be pushed onto or removed from the list of
+// selected items when the checkbox is clicked.
+// The "selected-list" attribute is an array which will contain the IDs
+// of selected records.
+// The "all-selected" attribute will be set to true or false depending
+// on whether or not all checkboxes in the checklist are checked.
+// The "record-count" attribute is the total count of records, used to
+// determine whether all records are selected or not.
 directive('checklist', ['$compile', '$parse', function($compile, $parse) {
   return {
     restrict: 'A',
